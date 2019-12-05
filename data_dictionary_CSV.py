4ce9c46adddd93fbd4a79422c89675c767e7a2da
stphnmrgn <stephenmorganj@gmail.com>
2019-12-05 12:22:09 -0500

    mvoed scripts from script folder

---
+++ data_dictionary_CSV.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+"""
+Title:         	data_dictionary_CSV.py
+Python:         2.7.8
+Purpose:     	Process for extracting information of features in a gdb
+Description: 	This script will return feature descriptions and then write 
+                them in a .csv file. User needs to input pathway of a 
+                geodatabase.
+Type: 		    Standalone script
+Author:      	Stephen Morgan, GISP
+Date Created:   09/24/2017 
+"""
+
+# Import modules
+import os
+import csv
+import arcpy
+from arcpy import env
+
+
+# Get user's workspace
+workspace = input('Type/paste the full pathway to your GDB folder, and then press Enter.'
+                   ' \nExample, r"C:Project\GIS\MyData.gdb": ')
+
+env.workspace = workspace # Set env.workspace to user's workspace
+fullname = os.path.basename(workspace) # Return base name of pathway (name of folder)
+name = fullname.strip(".gdb" ) # Strip extension off workspace name
+path_csv = os.path.dirname(workspace) + os.sep + name + "_datadictionary.csv" # Store folder name as string
+
+print "\nYour geodatabase directory: \n" + os.path.dirname(workspace)
+print "\nThe name of your workspace: \n" + fullname
+print "\nThe path to the csv we're creating: \n" + path_csv
+
+
+def csv_template(folder, outputfile):
+    """
+    Given a folder path, create a csv template.
+
+    Parameters
+    ----------
+    folder: string
+            folder pathway
+    outputfile: string
+            pathway & filename
+
+    Returns
+    -------
+    result: template csv file saved using the user specified file name
+    and folder destination.
+    """
+
+     with open(outputfile, "ab") as f:
+         print "\nCreating template csv file...\n"
+         w = csv.writer(f)
+         # Set variable to include column headers of the csv
+         header = ("Feature_Dataset", "Feature_Class", "Shape_Type", "Populated", 
+                    "Feature_Count", "Workspace", "Spatial_Reference", "Editor")
+         w.writerow(header)
+         rows = describe_data(folder)
+         w.writerows(rows)
+
+
+def describe_data(folder):
+     print "\nReading & Writing Feature Descriptions... \n"
+     # Create a list of feature classes and datasets
+     fcList = arcpy.ListFeatureClasses()
+     datasetList = arcpy.ListDatasets('*', 'Feature')
+     
+     # Loop through feature classes
+     for fc in fcList:
+         print "    feature: " + fc
+
+        # Describe object of gdb from user's workspace, return gdb name
+         desc = arcpy.Describe(workspace)
+         gdb = desc.name
+         # Describe object of feature classes
+         desc = arcpy.Describe(fc)
+         sr = desc.spatialReference.name
+         st = desc.shapeType
+         # So Editor Tracking objects can be returned
+         dt = desc.dataType
+
+         # If feature class has editor tracker enabled, return editorFieldName
+         if desc.editorTrackingEnabled:
+             who = desc.editorFieldName
+            # Use a cursor to search through editorFieldName of all features
+            # and create a dictionary of the user's name
+             userDictionary = {}
+             cur = arcpy.da.SearchCursor(fc, [who])
+             for row in cur:
+                 featureEditedBy = row[0]
+                 if featureEditedBy in userDictionary:
+                     userDictionary[featureEditedBy] += 1
+                 else:
+                     userDictionary[featureEditedBy] = 1
+             for user in list(userDictionary.keys()):
+                # If editor tracking is on and editorFieldName is None (no data),
+                # then user's set to "Null", else field is set to the user's name
+                 if user == None:
+                    who = "Null"
+                 else:
+                    who = user
+         else:
+             who = ("Editor tracker not enabled")
+
+        # Get number of features in feature classes first
+        # If feature count of first attribute field = zero then
+        # feature is not populated, else the feature is populated        
+         fc_count = arcpy.management.GetCount(fc)
+         if fc_count[0] == "0":
+             pop = "False"
+         else:
+             pop = "True"
+
+        # Put all these variables in order for the .csv file
+         seq = ('"Not in Feature Dataset"', fc, st, pop, fc_count, gdb, sr, who)
+         yield seq
+
+     # Same, except for features within a dataset
+     for dataset in datasetList:
+         setList = arcpy.ListFeatureClasses("*", "", dataset)
+         
+         for fc in setList:
+             print "    feature: " + fc
+
+             desc = arcpy.Describe(workspace)
+             gdb = desc.name
+             desc = arcpy.Describe(fc)
+             st = desc.shapeType
+             sr = desc.spatialReference.name
+             dt = desc.dataType
+
+             if desc.editorTrackingEnabled:
+                 who = desc.editorFieldName
+                 userDictionary = {}
+                 cur = arcpy.da.SearchCursor(fc, [who])
+                 for row in cur:
+                     featureEditedBy = row[0]
+                     if featureEditedBy in userDictionary:
+                         userDictionary[featureEditedBy] += 1
+                     else:
+                         userDictionary[featureEditedBy] = 1
+                 for user in list(userDictionary.keys()):
+                     if user == None:
+                         who = "Null"
+                     else:
+                         who = user
+             else:
+                 who = ("Editor tracker not enabled")
+
+             fc_count = arcpy.management.GetCount(fc)
+             if fc_count[0] == "0":
+                 pop = "False"
+             else:
+                 pop = "True"
+
+             seq = (dataset, fc, st, pop, fc_count, gdb, sr, who)
+             yield seq
+
+
+if __name__ == "__main__":
+    folderPath = workspace      # or arcpy.GetParameterAsText(0)
+    output = path_csv           # or arcpy.GetParameterAsText(1)
+    csv_template(folderPath, output)
+
+
+print "\nSpam and Eggs are Done\n"
---
+++ mxd_brokenlink_CSV.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+Title:          mxd_brokenlink_CSV.py
+Python:         2.7.8
+Purpose: 	    List broken data sources in MXDs and write to a .csv file
+Description: 	This script will output a csv file indicating the layers
+                with broken data sources within MXDs in a given folder.
+                User inputs pathway to map folder.
+Type: 		    Standalone script
+Author: 	    Stephen Morgan, GISP
+Date Created: 	09/26/2017
+"""
+
+# Import modules
+import os
+import csv
+from arcpy import mapping
+import time
+
+
+# Get user's map folder pathway
+workspace = input('Type/paste the full pathway to your maps folder, and then press Enter.'
+                  ' \nExample, r"C:Project\GIS\Maps": ')
+
+fullname = os.path.basename(workspace) # Return base name of pathway (name of folder)
+path_csv = os.path.dirname(workspace) + os.sep + fullname + "_mxdbrokenlinks.csv" # Store folder name as string
+
+print "\nYour map directory: \n" + os.path.dirname(workspace)
+print "\nThe name of your map folder: \n" + fullname
+print "\nThe path of csv we're creating: \n" + path_csv
+
+
+def csv_template(folder, outputfile):
+    """
+    Given a folder path, create a csv template.
+
+    Parameters
+    ----------
+    folder: string
+            folder pathway
+    outputfile: string
+            pathway & filename
+
+    Returns
+    -------
+    result: template csv file saved using the user specified file name
+    and folder destination.
+    """
+    
+    with open(outputfile, "wb") as f:
+        print "\nCreating template csv file...\n"
+        w = csv.writer(f)
+        # Set variable to include the column headers of the csv
+        header = ("Map_Document", "Broken_Layer", "Layer_Data_Source")
+        w.writerow(header)
+        rows = mxd_broken_links(folder)
+        w.writerows(rows)
+
+
+def mxd_broken_links(folder):
+    """
+    Given a folder path, search folder directory for mxd files.
+
+    Parameters
+    ----------
+    folder: string
+            folder pathway
+
+    Returns
+    -------
+    result: A sequence of feature class information to store in csv file. For each
+    broken layer found within a MXD file, the following information is stored:
+    
+        map name: string
+        broken layer: string
+        broken layer data path: string
+    """
+
+    for root, dirs, files in os.walk(folderPath):
+        for fileName in files:
+            # Crawl folder for files with .mxd extention
+            if fileName.lower().endswith(".mxd"):
+                # Return MXD pathways
+                fullPath = os.path.join(root, fileName)
+                # Return MXD properties
+                mxd = mapping.MapDocument(fullPath)
+                print "\nChecking MXD: " + fileName
+
+                # Loop broken layers in MXDs, returning a list
+                brokenlinks = mapping.ListBrokenDataSources(mxd)
+                if brokenlinks == None:
+                    fileName = "No broken links were found"
+                    linkname = "No broken links were found"
+                    DataSource = "No broken links were found"
+                    seq = (fileName, linkname, DataSource)
+                    yield seq
+                else:
+                    for brokenlink in brokenlinks:
+                        print "Broken Data Link: " + brokenlink.name
+                        linkname = brokenlink.name
+                        DataSource = brokenlink.dataSource
+                        seq = (fileName, linkname, DataSource)
+                        yield seq
+                del mxd
+                
+            else:
+                #fileName = "No MXDs were found in folder"
+                linkname = "File is not a MXD"
+                DataSource = os.path.join(root, fileName)
+                seq = (fileName, linkname, DataSource)
+                yield seq
+                print "\nFile(s) is not a MXD: " + fileName
+
+
+if __name__ == "__main__":
+    folderPath = workspace      # or arcpy.GetParameterAsText(0)
+    output = path_csv           # or arcpy.GetParameterAsText(1)
+    csv_template(folderPath, output)
+
+time.sleep(6)
+
+print "\nSpam and Eggs are Done\n"
