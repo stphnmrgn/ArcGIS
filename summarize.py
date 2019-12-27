3885534d03b544b9ba9913682a7dab23dba20c93
stphnmrgn <stephenmorganj@gmail.com>
2019-12-27 14:38:43 -0500

    none

--- summarize.py
+++ summarize.py
@@ -3,21 +3,21 @@
 # import system modules 
 import arcpy
 
-# Set environment settings
-arcpy.env.workspace = 'C:/data/city.gdb'
+# # Set environment settings
+# arcpy.env.workspace = 'C:/data/city.gdb'
 
-#  Set local variables
-polys = 'neighborhoods'
-points = 'crimes'
-outFeatureClass = 'crimes_aggregated'
-keepAll = 'KEEP_ALL'
-sumFields = [['Damages', 'SUM'], ['VICTIM_AGE', 'MEAN']]
-addShapeSum = 'ADD_SHAPE_SUM'
-groupField = 'Crime_type'
-addMinMaj = 'ADD_MIN_MAJ'
-addPercents = 'ADD_PERCENT'
-outTable = 'crimes_aggregated_groups'
+# #  Set local variables
+# polys = 'neighborhoods'
+# points = 'crimes'
+# outFeatureClass = 'crimes_aggregated'
+# keepAll = 'KEEP_ALL'
+# sumFields = [['Damages', 'SUM'], ['VICTIM_AGE', 'MEAN']]
+# addShapeSum = 'ADD_SHAPE_SUM'
+# groupField = 'Crime_type'
+# addMinMaj = 'ADD_MIN_MAJ'
+# addPercents = 'ADD_PERCENT'
+# outTable = 'crimes_aggregated_groups'
 
-arcpy.SummarizeWithin_analysis(polys, points, outFeatureClass, keepAll, 
-                               sumFields, addShapeSum, '', groupField, 
-                               addMinMaj, addPercents, outTable)
+# arcpy.SummarizeWithin_analysis(polys, points, outFeatureClass, keepAll, 
+#                                sumFields, addShapeSum, '', groupField, 
+#                                addMinMaj, addPercents, outTable)
