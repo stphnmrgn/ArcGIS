649c6ac0d6454dceaf94e2a66f65310236710734
stphnmrgn <stephenmorganj@gmail.com>
2019-12-30 18:06:51 -0500

    testing arcpy summarize module

--- summarize.py
+++ summarize.py
@@ -3,21 +3,23 @@
 # import system modules 
 import arcpy
 
-# # Set environment settings
-# arcpy.env.workspace = 'C:/data/city.gdb'
 
-# #  Set local variables
-# polys = 'neighborhoods'
-# points = 'crimes'
-# outFeatureClass = 'crimes_aggregated'
-# keepAll = 'KEEP_ALL'
-# sumFields = [['Damages', 'SUM'], ['VICTIM_AGE', 'MEAN']]
-# addShapeSum = 'ADD_SHAPE_SUM'
+# # Set environment settings
+arcpy.env.workspace = 'C:\Users\607644\Documents\Projects\VBA\data\created\'
+
+#  Set local variables
+polys = 'C:\Users\607644\Documents\GIS\Data\census_tiger\tl_2018_us_county\tl_2018_us_county.shp'
+points = 'defrauded_ip_pts.shp'
+outFeatureClass = 'fraudpts_aggregated_us_county2.shp'
+keepAll = 'KEEP_ALL'
+sumFields = [['S_AT_RISK', 'SUM'], ['S_LOST', 'SUM'], ['COUNT', 'SUM'], ['S_RECOVERE', 'SUM'], ['S_PROTECTE', 'SUM'], ['S_RETRO_AT', 'SUM'], ['S_RETRO_LO', 'SUM'], ['S_RETRO_RE', 'SUM'], ['S_RETRO_PR', 'SUM']]
+addShapeSum = 'ADD_SHAPE_SUM'
 # groupField = 'Crime_type'
 # addMinMaj = 'ADD_MIN_MAJ'
 # addPercents = 'ADD_PERCENT'
 # outTable = 'crimes_aggregated_groups'
 
+
+# arcpy.SummarizeWithin()
 # arcpy.SummarizeWithin_analysis(polys, points, outFeatureClass, keepAll, 
-#                                sumFields, addShapeSum, '', groupField, 
-#                                addMinMaj, addPercents, outTable)
+#                                sumFields, addShapeSum)
